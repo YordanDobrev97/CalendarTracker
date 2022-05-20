3bc8e3877c11087a8d4b5db52f35f5c2c12609a7
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-05-21 00:08:38 +0300

    Integrate google login

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
-const { engine } = require("express-handlebars");
+const { engine } = require("express-handlebars")
+const session = require('express-session')
 const router = require('./controllers/routes')
 const PORT = 5000
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
 
 app.engine(
   "hbs",
@@ -10,7 +17,50 @@ app.engine(
     extname: "hbs",
   })
 );
+
 app.set("view engine", "hbs");
+
+const passport = require('passport');
+var userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+ 
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '633753943075-n8g4csf68ij9varq4s6e66urrbenvls8.apps.googleusercontent.com'
+const GOOGLE_CLIENT_SECRET = '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'
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success');
+  });
+
 app.use('/', router)
 
 app.listen(PORT, () => console.log(`Server listening at ${PORT} port...`))

1a6941ebf0efa4a9c49527d157972b58f115638f
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-05-20 23:22:32 +0300

    Add simple styles

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const { engine } = require("express-handlebars");
+const router = require('./controllers/routes')
 const PORT = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.use('/', router)
 
 app.listen(PORT, () => console.log(`Server listening at ${PORT} port...`))
---
+++ utils/credentials.js
@@ -0,0 +1,7 @@
+const clientId = '633753943075-n8g4csf68ij9varq4s6e66urrbenvls8.apps.googleusercontent.com'
+const clientSecret = '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'
+
+module.exports = {
+  clientId,
+  clientSecret
+}
